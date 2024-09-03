c8cbca28f4cd15b03c78d5bb0c6cf1b559dbfa40
costadonifabricio <fabriciocostadoni@gmail.com>
2024-09-03 09:49:12 -0300

    Trabajo terminado

--- README.md
+++ README.md
@@ -27,3 +27,12 @@ Puedes instalar las dependencias necesarias utilizando pip:
 ```bash
 pip install pandas numpy matplotlib tensorflow keras
 ```
+
+## Visualización de Resultados:
+El modelo arrojaba los siguientes resultados con learning_rate a 0.0004:
+![alt text](<Captura de pantalla 2024-09-03 093657.png>)
+
+El modelo arrojaba los siguientes resultados con leraning_rate a 0.00003:
+El ECM se calcula para evaluar la precisión del modele.
+Datos Originales y Regresión Lineal, compara los datos reales con la línea de regresión predicha. El resultado muestra el peso predicho para una persona con una altura de 170 cm.
+![alt text](<Captura de pantalla 2024-09-03 093818.png>)
--- main.py
+++ main.py
@@ -7,42 +7,59 @@ from keras.api.optimizers import SGD
 
 ## Lee el archivo CSV y devuelve las variables x (altura) y y (peso).
 def cargar_datos(ruta_csv):
-    df = pd.read_csv(ruta_csv)
-    x = df['Altura'].values
-    y = df['Peso'].values
+    datos = pd.read_csv(ruta_csv, sep=',', header=0)
+    x = datos["Altura"].values
+    y = datos["Peso"].values
     return x, y
 
 ## Crea y compila un modelo de Regresión Lineal usando Keras.
-def crear_modelo():
-    model = Sequential()
-    model.add(Dense(1, input_dim=1, activation='linear'))
-    model.compile(optimizer=SGD(learning_rate=0.0004), loss='mean_squared_error')
-    return model
+def crear_modelo(input_dim, output_dim, learning_rate):
+    np.random.seed(2)
+    modelo = Sequential()
+    modelo.add(Dense(output_dim, input_dim=input_dim, activation='linear'))
+    sgd = SGD(learning_rate=learning_rate)
+    modelo.compile(loss='mse', optimizer=sgd)
+    modelo.summary()
+    return modelo
 
 ## Entrena el modelo con los datos de altura (x) y peso (y).
-def entrenar_modelo(model, x, y, epochs=100):
-    model.fit(x, y, epochs=epochs, verbose=1)
-    return model
-
-## Realiza predicciones con el modelo entrenado.
-def realizar_predicciones(model, x):
-    return model.predict(x)
+def entrenar_modelo(modelo, x, y, num_epochs, batch_size):
+    historia = modelo.fit(x=x, y=y, epochs=num_epochs, batch_size=batch_size, verbose=1)
+    return historia
 
 ## Muestra los resultados de la predicción comparados con los datos reales.
-def visualizar_resultados(x, y, y_pred):
+def visualizar_resultados(historia, x, y, y_regr):
+    plt.subplot(1, 2, 1)
+    plt.plot(historia.history['loss'])
+    plt.xlabel('epoch')
+    plt.ylabel('ECM')
+    plt.title('ECM vs. epochs')
 
-    plt.scatter(x, y, color='blue', label='Datos reales')
-    plt.plot(x, y_pred, color='red', label='Predicción')
-    plt.title('Regresión Lineal entre Altura y Peso')
-    plt.xlabel('Altura')
-    plt.ylabel('Peso')
-    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.scatter(x, y)
+    plt.plot(x, y_regr, color='red', label='linea de predicción')
+    plt.title('Datos originales y regresión lineal')
     plt.show()
+
+## Realiza una predicción para una altura específica.
+def realizar_prediccion(modelo, x_pred):
+    y_pred = modelo.predict(x_pred)
+    return y_pred
 
 # Ejecución del código
 ruta_csv = 'altura_peso.csv'
 x, y = cargar_datos(ruta_csv)
-model = crear_modelo()
-model = entrenar_modelo(model, x, y)
-y_prediccion = realizar_predicciones(model, x)
-visualizar_resultados(x, y, y_prediccion)
+modelo = crear_modelo(input_dim=1, output_dim=1, learning_rate=0.00003)
+historia = entrenar_modelo(modelo, x, y, num_epochs=100, batch_size=len(x))
+
+capas = modelo.layers[0]
+w, b = capas.get_weights()
+print('Parámetros: w = {:.1f}, b = {:.1f}'.format(w[0][0], b[0]))
+
+y_regr = modelo.predict(x)
+print('ECM = {:.2f}'.format(np.mean((y_regr - y) ** 2)))
+visualizar_resultados(historia, x, y, y_regr)
+
+x_pred = np.array([170])
+y_pred = realizar_prediccion(modelo, x_pred)
+print(f"Peso predicho: {y_pred[0][0]:.2f} kg")
